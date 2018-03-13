66f2d231a43a8e27f4992eda47c81c39f538bb9f
Natalie Nuno <natalienuno@gmail.com>
2018-03-13 10:26:30 -0700

    Initial Commit

---
+++ rainbow_road/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainbow_road
+//
+//  Created by Natalie Nuno on 3/13/18.
+//  Copyright © 2018 Natalie Nuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
